8462a3a1acbe3e1987aa24d5840eb881a9048760
stifler97 <stifler@binshops.com>
2022-08-04 07:07:30 -0400

    feat: calculate cheapest product based on category

--- controllers/front/list.php
+++ controllers/front/list.php
@@ -1,12 +1,41 @@
 <?php
 
-use function Symfony\Component\DependencyInjection\Loader\Configurator\ref;
+use FacebookAds\Object\BusinessDataAPI\Content;
 
 class DummymodulenameListModuleFrontController extends ModuleFrontController
 {
     public function init()
     {
         parent::init();
-        echo 'heelo';
+        header('Content-Type: application/json');
+        $response = [];
+
+        $id_language = (int) Tools::getValue('id_language', $this->context->language->id);
+        $id_shop = (int) Tools::getValue('id_shop', $this->context->shop->id);
+
+        $id_category = (int) Tools::getValue('id_category');
+
+        $category = new Category($id_category);
+        if(!$id_category
+            or !Validate::isLoadedObject($category)) {
+            $category = Category::getRootCategory($id_language);
+        }
+        
+        $cheapestProduct = $category->getProducts(
+            $this->context->language->id,
+            1,
+            1,
+            'price',
+            'asc'
+        );
+
+        $cheapestProduct = array_pop($cheapestProduct);
+        
+        $response['price_start_at_in_this_category'] = $cheapestProduct['price'];
+        $response['cheapest_product'] = $cheapestProduct;
+        $response['category'] = $category;
+
+        echo json_encode($response);
+        die;
     }
 }
